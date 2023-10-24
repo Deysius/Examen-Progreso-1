e01fcda3178cdfb9ad064e0088092d2b0036ff48
Josue Ayala <josue.ayala.flores@udla.edu.ec>
2023-10-24 15:00:47 -0500

    Agrego otra variable para que sirva de contador, y realizo la operacion con un bucle

--- Examen-Progres.c
+++ Examen-Progres.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 int main(){
 int num;
-int hexadecimal;
-printf ("Ingrese un numero\n");
+int hexadecimal=0;
+int contador=0;
+printf ("Ingrese un numero para encontrar el numero hexadecimal\n");
 scanf ("%d",&num);
 //Inicializo mis variables, y solicito al usuario que ingrese el valor de una de las variables
+while (hexadecimal<num)
+{
+}
+printf ("%dD",contador);
 return 0;
 }
