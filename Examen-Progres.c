0ca9f81e87abe709c8856c194e2ea3d2f2702460
Josue Ayala <josue.ayala.flores@udla.edu.ec>
2023-10-24 15:09:40 -0500

    Se agrega un ifelse para que compare si es que el numero del contador es el hexadecimal

--- Examen-Progres.c
+++ Examen-Progres.c
@@ -3,6 +3,7 @@ int main(){
 int num;
 int hexadecimal=0;
 int contador=0;
+int hexa=16;
 printf ("Ingrese un numero para encontrar el numero hexadecimal\n");
 scanf ("%d",&num);
 //Inicializo mis variables, y solicito al usuario que ingrese el valor de una de las variables
@@ -11,5 +12,12 @@ while (hexadecimal<num)
 contador=contador+1;
 hexadecimal=16*contador;
 }
+if (hexa*contador==num)
+{
+    contador=contador;
+}
+else
+contador=contador-1;
+printf("%dD",contador);
 return 0;
 }
