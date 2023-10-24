286235e597a27db47c9b93fa9046230e67e7b1f7
Josue Ayala <josue.ayala.flores@udla.edu.ec>
2023-10-24 14:25:19 -0500

    Inicializo mis variables

---
+++ Examen-Progres.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+int num;
+int hexadecimal;
+printf ("Ingrese un numero\n");
+scanf ("%d",&num);
+//Inicializo mis variables, y solicito al usuario que ingrese el valor de una de las variables
+return 0;
+}
